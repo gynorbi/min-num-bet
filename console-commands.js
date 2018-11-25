f3d3f9aae05da48ed79ecbe7693ead2c0c5ce947
Norbert Gyarfas <gynorbi@gmail.com>
2018-11-25 18:20:50 +0100

    Changed some console commands

--- console-commands.js
+++ console-commands.js
@@ -1,4 +1,4 @@
-MinNumBet.deployed().then(function(instance){return instance.createNewSession(3);});
+MinNumBet.deployed().then(function(instance){return instance.createNewSession(web3.toWei(1.2,'ether'));});
 MinNumBet.deployed().then(function(instance){return instance.placeBet(0,5,{value:web3.toWei(3,'ether')});});
 MinNumBet.deployed().then(function(instance){return instance.placeBet(0,4,{value:web3.toWei(3,'ether'),from:"0xA522309b1b164fDd464117493DF604c07972c1af"});});
 MinNumBet.deployed().then(function(instance){return instance.placeBet(0,4,{value:web3.toWei(3,'ether'),from:"0xFF21Bb983BF6a8d569ab9d486a7E58a88aE46586"});});
@@ -8,4 +8,4 @@ MinNumBet.deployed().then(function(instance){return instance.closeSession(0);});
 MinNumBet.deployed().then(function(instance){return instance.getWinner(0);});
 MinNumBet.deployed().then(function(instance){return instance.withdraw(0,{from:"0xe3bf1727e73d2d24dc5a9febea491a752a8c9895"});});
 
-web3.fromWei(web3.eth.getBalance("0x213075181111fD850c3aEFDa3Cb41fafcd238DCb"),'ether').toString()
+web3.fromWei(web3.eth.getBalance("0x55A8a30De87DE8a2EfBc781cf99B637B4BdCa74d"),'ether').toString()
