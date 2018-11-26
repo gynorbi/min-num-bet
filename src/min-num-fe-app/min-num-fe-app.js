eef72a748486b9b41efa08093a21e8fd1c388a42
Norbert Gyarfas <gynorbi@gmail.com>
2018-11-26 23:12:30 +0100

    Found workaround for event watching.

--- src/min-num-fe-app/min-num-fe-app.js
+++ src/min-num-fe-app/min-num-fe-app.js
@@ -14,6 +14,7 @@ import '@polymer/paper-card/paper-card.js';
 import '@polymer/app-layout/app-layout.js';
 
 import '@polymer/paper-dialog/paper-dialog.js';
+import '../session-details/session-details.js';
 
 
 /**
@@ -45,34 +46,11 @@ class MinNumFeApp extends PolymerElement {
           max-width: 1000px;
           margin: 5px auto;
         }
-        .card-container {
-          display: inline-block;
-          width: 33.33%;
-          color: black;
-          text-decoration: none;
-        }
-        paper-card {
-          display: block;
-          margin: 5px;
-        }
-        paper-card h2 {
-          margin: 4px;
-          font-weight: normal;
-        }
-        paper-card p {
-          margin: 4px;
-          color: #999;
-        }
-        
         @media (max-width: 960px) {
           .content {
             max-width: 800px;
           }
-          .card-container {
-            width: 50%;
-          }
         }
-
         @media (max-width: 719px) {
           app-toolbar {
             height: 60px;
@@ -83,9 +61,6 @@ class MinNumFeApp extends PolymerElement {
           }
           .content {
             max-width: 400px;
-          }
-          .card-container {
-            width: 100%;
           }
         }
       </style>
@@ -100,49 +75,27 @@ class MinNumFeApp extends PolymerElement {
           </app-header>
           <div class="content">
             <template is="dom-repeat" items=[[sessions]] as="session">
-              <paper-card>
-              <div class="card-content">
-                <iron-icon icon="[[getSessionStatus(session)]]" slot="item-icon"></iron-icon>
-                  <span><b>Owner: [[session.owner]]</b></span> <span>Bet of this session: [[transformToEther(session.value)]] ETH</span>
-                  <span>Winner: [[session.winner]]</span>
-                  <iron-collapse opened="{{session.selected || 1}}">
-                    <h3>Bets</h3>
-                    <table>
-                      <thead>
-                        <tr>
-                          <th>Player</th>
-                          <th>Bet</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                      <template is="dom-repeat" items=[[session.bets]] as="bet">
-                        <tr>
-                          <td>[[bet.player]]</td>
-                          <td>[[showBet(bet.bet)]]</td>
-                        </tr>
-                      </template>
-                      </tbody>
-                    </table>
-                  </iron-collapse>
-                  <div>
-                    <paper-input label="Your bet" value={{session.newBet}} required  auto-validate pattern="[0-9]*" error-message="Numbers only" invalid="{{session.invalidBet}}"></paper-input>
-                    <paper-button raised on-click="submitBet" session-id="[[session.id]]" bet="[[session.newBet]]" value="[[session.value]]" disabled="[[disableBet(session.invalidBet, session.newBet.length)]]">Bet</paper-button>
-                    <paper-button raised on-click="closeSession" session-id="[[session.id]]" disabled="[[disableCloseSession(session.isOpen,session.owner)]]">Close session</paper-button>
-                    <paper-button raised on-click="withdraw" session-id="[[session.id]]" disabled="[[disableWithdraw(session.hasBeenPaid, session.winner)]]">Withdraw</paper-button>
-                  </div>
-              </div>
-              </paper-card>
+              <session-details tabindex="-1"
+                id="[[session.id]]"
+                is-open="[[session.isOpen]]" 
+                owner="[[session.owner]]"
+                winner="[[session.winner]]"
+                session-value="[[session.value]]"
+                has-been-paid="[[session.hasBeenPaid]]"
+                bets="[[session.bets]]"
+                >
+              </session-details>
             </template>
           </div>
         </app-header-layout>
         <paper-dialog id="newSession" modal on-iron-overlay-closed="createSession" on-iron-overlay-opened="cleanupNewSessionDialog" value="[[sessionValue]]">
           <h2>Create new session</h2>
-          <p>Specify here how much you would like each player to bet in this session</p>
-          <paper-input id="betInput" autofocus required auto-validate pattern="[0-9]*" label="Bet in Wei" value="{{sessionValue}}" invalid={{invalidBet}}></paper-input>
-          <paper-input disabled label="Bet in ether" value="[[transformToEther(sessionValue)]]"></paper-input>
+          <p>Specify here how much you would like each player to put as stake next to his bet in this session</p>
+          <paper-input id="stakeInput" autofocus required auto-validate pattern="[0-9]*" label="Stake in Wei" value="{{sessionValue}}" invalid={{invalidStake}}></paper-input>
+          <paper-input disabled label="Stake in ether" value="[[transformToEther(sessionValue)]]"></paper-input>
           <div class="buttons">
             <paper-button dialog-dismiss>Cancel</paper-button>
-            <paper-button dialog-confirm disabled=[[invalidBet]] >Create session</paper-button>
+            <paper-button dialog-confirm disabled=[[invalidStake]] >Create session</paper-button>
           </div>
         </paper-dialog>
         <paper-fab icon="add" on-tap="openNewSessionDialog"></paper-fab>
@@ -152,7 +105,8 @@ class MinNumFeApp extends PolymerElement {
   static get properties() {
     return {
       sessions: {
-        type: Array
+        type: Array,
+        value:[]
       },
       web3Provider: {
         type: Object,
@@ -180,8 +134,7 @@ class MinNumFeApp extends PolymerElement {
     await this.initContract();
     this.currentUserAccount = web3.eth.accounts[0];
     this.sessions = await this.initSessionList();
-    //this.addEventListener(this.bettingInstance.NewBet,this.updateBet);
-    this.bettingInstance.NewBet({sessionId:-1,playerId:-1},this.updateBet.bind(this));
+    this.bettingInstance.NewSession(this.updateSessions.bind(this));    
   }
   async initWeb3() {
     // Modern dapp browsers...
@@ -246,49 +199,7 @@ class MinNumFeApp extends PolymerElement {
       bets: bets
     }
   }
-  getSessionStatus(session) {
-    return session.isOpen ? "check" : session.hasBeenPaid ? "euro-symbol" : "block";
-  }
-  isThereWinner(session) {
-    return session.winner !== '0x0000000000000000000000000000000000000000';
-  }
-  showBet(betAmmount) {
-    return betAmmount < 0 ? "*****" : betAmmount;
-  }
-  disableBet(isBetInvalid, betInputLength) {
-    if (isBetInvalid === undefined) return true;
-    return isBetInvalid || betInputLength < 1;
-  }
-  disableCloseSession(isOpen, owner) {
-    return !isOpen || owner !== web3.eth.accounts[0];
-  }
-  disableWithdraw(hasBeenPaid, winner) {
-    return hasBeenPaid || winner !== web3.eth.accounts[0];
-
-  }
-  async updateBet(error, event) {
-    if (!error) {
-      var sessionId = event.args.sessionId.toNumber();
-      var playerId = event.args.playerId.toNumber();
-      if (sessionId > -1 && playerId > -1) {
-        var playerData = await this.bettingInstance.getPlayerData(sessionId, playerId);
-        this.sessions[sessionId].bets.push({ player: playerData[0], bet: playerData[1].toNumber() });
-      }
-    }
-  }
-  async submitBet(event) {
-    var sessionId = event.currentTarget.sessionId;
-    var bet = event.currentTarget.bet;
-    var value = event.currentTarget.value;
-    console.log(`Bet: ${bet}, SessionId: ${sessionId}, Value in ether: ${value}`);
-    try {
-      await this.bettingInstance.placeBet(sessionId, bet, { value: value });
-    }
-    catch (error) {
-      console.log("Somthing went wrong: " + error);
-    }
-  }
-
+  
   async createSession(event) {
     var value = event.currentTarget.value;
     event.currentTarget.value = "";
@@ -302,38 +213,26 @@ class MinNumFeApp extends PolymerElement {
     }
   }
 
+  async updateSessions(error,event){
+    if (!error) {
+      var sessionId = event.args.sessionId.toNumber();
+      if (sessionId >= this.sessions.length) {
+        var sessionData = await this.getSessionData(sessionId);
+        this.push('sessions',sessionData);
+      }
+    }
+  }
+
   openNewSessionDialog() {
     this.$.newSession.open();
   }
 
   cleanupNewSessionDialog(){
-    this.$.betInput.value="";
+    this.$.stakeInput.value="";
   }
 
-  transformToEther(betInWei){
-    return web3.fromWei(betInWei,'ether');
-  }
-
-  async closeSession(event) {
-    var sessionId = event.currentTarget.sessionId;
-    console.log(`Closing session with id '${sessionId}'.`);
-    try {
-      await this.bettingInstance.closeSession(sessionId);
-    }
-    catch (error) {
-      console.log("Somthing went wrong: " + error);
-    }
-  }
-
-  async withdraw(event) {
-    var sessionId = event.currentTarget.sessionId;
-    console.log(`Withdrawing winnings form session with id '${sessionId}'.`);
-    try {
-      await this.bettingInstance.withdraw(sessionId);
-    }
-    catch (error) {
-      console.log("Somthing went wrong: " + error);
-    }
+  transformToEther(numberInWei){
+    return web3.fromWei(numberInWei,'ether');
   }
 }
 
--- src/session-details/session-details.js
+++ src/session-details/session-details.js
@@ -3,6 +3,8 @@ import '@polymer/iron-icon/iron-icon.js';
 import '@polymer/iron-icons/iron-icons.js';
 import '@polymer/paper-item';
 import '@polymer/iron-collapse/iron-collapse.js';
+import '@polymer/iron-ajax/iron-request.js';
+import '@polymer/paper-card/paper-card.js';
 
 
 /**
@@ -42,49 +44,87 @@ class SessionDetails extends PolymerElement {
         tbody td {
           text-align: left;
         }
+        
+        .card-container {
+          display: inline-block;
+          width: 33.33%;
+          color: black;
+          text-decoration: none;
+        }
+        paper-card {
+          display: block;
+          margin: 5px;
+        }
+        paper-card h2 {
+          margin: 4px;
+          font-weight: normal;
+        }
+        paper-card p {
+          margin: 4px;
+          color: #999;
+        }
+        @media (max-width: 960px) {
+          .card-container {
+            width: 50%;
+          }
+        }
+
+        @media (max-width: 719px) {
+          .card-container {
+            width: 100%;
+          }
+        }
       </style>
-      <session-details tabindex="-1"
-            id="[[item.id]]"
-            is-open="[[item.isOpen]]" 
-            owner="[[item.owner]]"
-            winner="[[item.winner]]"
-            value="[[item.value]]"
-            has-been-paid="[[item.hasBeenPaid]]"
-            bets="[[item.bets]]"
-            >
-          </session-details>
-      <paper-icon-item focused="{{selected}}">
-        <iron-icon icon="[[getSessionStatus()]]" slot="item-icon"></iron-icon>
-        <paper-item-body>
-          <span><b>Owner: [[owner]]</b></span> <span>Bet of this session: [[value]] ETH</span>
-          <dom-if if="[[isThereWinner()]]">
+      <iron-request id="getContract"></iron-request>
+      
+      <paper-card>
+        <div class="card-content">
+          <iron-icon icon="[[getSessionStatus()]]" slot="item-icon"></iron-icon>
+            <span><b>Owner: [[owner]]</b></span> <span>Stake of this session: [[transformToEther(sessionValue)]] ETH</span>
             <span>Winner: [[winner]]</span>
-          </dom-if>
-          <iron-collapse opened="[[selected]]">
-            <h3>Bets</h3>
-            <table>
-              <thead>
-                <tr>
-                  <th>Player</th>
-                  <th>Bet</th>
-                </tr>
-              </thead>
-              <tbody>
-              <template is="dom-repeat" items=[[bets]]>
-                <tr>
-                  <td>[[item.player]]</td>
-                  <td>[[showBet(item.bet)]]</td>
-                </tr>
-              </template>
-              </tbody>
-            </table>
-          </iron-collapse>
-        </paper-item-body>
-      </paper-icon-item>
+            <iron-collapse opened="{{selected || 1}}">
+              <h3>Bets</h3>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Player</th>
+                    <th>Bet</th>
+                  </tr>
+                </thead>
+                <tbody>
+                <template is="dom-repeat" items=[[bets]] as="bet">
+                  <tr>
+                    <td>[[bet.player]]</td>
+                    <td>[[showBet(bet.bet)]]</td>
+                  </tr>
+                </template>
+                </tbody>
+              </table>
+            </iron-collapse>
+              <div>
+                <paper-input id="sendBet" label="Your bet" value={{currentBet}} required  auto-validate pattern="[0-9]*" error-message="Numbers only" invalid="{{invalidBet}}"></paper-input>
+                <paper-button raised on-click="submitBet" session-id="[[id]]" bet="[[currentBet]]" session-value="[[sessionValue]]" disabled="[[disableBet(invalidBet, currentBet.length)]]">Bet</paper-button>
+                <paper-button raised on-click="closeSession" session-id="[[id]]" disabled="[[disableCloseSession(isOpen,owner)]]">Close session</paper-button>
+                <paper-button raised on-click="withdraw" session-id="[[id]]" disabled="[[disableWithdraw(hasBeenPaid, winner)]]">Withdraw</paper-button>
+              </div>
+        </div>
+      </paper-card>
     `;
   }
   static get properties() {
     return {
+      web3Provider: {
+        type: Object,
+        value: null
+      },
+      contracts: {
+        type: Object,
+        value: {}
+      },
+      bettingInstance: {
+        type: Object,
+        value: {}
+      },
       id: {
         type: Number
       },
@@ -97,7 +137,7 @@ class SessionDetails extends PolymerElement {
       winner: {
         type: String
       },
-      value: {
+      sessionValue: {
         type: Number
       },
       hasBeenPaid: {
@@ -108,14 +148,116 @@ class SessionDetails extends PolymerElement {
       }
     };
   }
-  getSessionStatus(){
-    return this.isOpen ? "check" : this.hasBeenPaid ? "euro-symbol":"block";
+
+  constructor(){
+    super();
+    this.init();
   }
-  isThereWinner(){
-    return !this.winner === '0x0000000000000000000000000000000000000000';
+  
+  async init() {
+    await this.initWeb3();
+    await this.initContract();
+    this.bettingInstance.NewBet({sessionId:this.id},{fromBlock:'latest'},this.updateBets.bind(this));
   }
-  showBet(bet){
-    return bet < 0 ? "*****" : bet;
+  async initWeb3() {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      this.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.enable();
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      this.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      this.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(this.web3Provider);
+  }
+  async initContract() {
+    var contractData = await this.$.getContract.send({ url: "/build/contracts/MinNumBet.json", handleAs: "json" });
+    // Get the necessary contract artifact file and instantiate it with truffle-contract
+    var MinNumBetArtifact = contractData.response;
+    this.contracts.MinNumBet = TruffleContract(MinNumBetArtifact);
+
+    // Set the provider for our contract
+    this.contracts.MinNumBet.setProvider(this.web3Provider);
+
+    // Set up the instance of the contract
+    this.bettingInstance = await this.contracts.MinNumBet.deployed();
+  }
+  getSessionStatus() {
+    return this.isOpen ? "check" : this.hasBeenPaid ? "euro-symbol" : "block";
+  }
+  isThereWinner() {
+    return this.winner !== '0x0000000000000000000000000000000000000000';
+  }
+  showBet(betAmmount) {
+    return betAmmount < 0 ? "*****" : betAmmount;
+  }
+  disableBet(isBetInvalid, betInputLength) {
+    if (isBetInvalid === undefined) return true;
+    return isBetInvalid || betInputLength < 1;
+  }
+  disableCloseSession() {
+    return !this.isOpen || this.owner !== web3.eth.accounts[0];
+  }
+  disableWithdraw() {
+    return this.hasBeenPaid || this.winner !== web3.eth.accounts[0];
+
+  }
+  transformToEther(betInWei){
+    return web3.fromWei(betInWei,'ether');
+  }
+  async updateBets(error, event) {
+    if (!error) {
+      var sessionId = event.args.sessionId.toNumber();
+      var playerId = event.args.playerId.toNumber();
+      if (playerId >= this.bets.length) {
+        var playerData = await this.bettingInstance.getPlayerData(sessionId, playerId);
+        this.push('bets',{ player: playerData[0], bet: playerData[1].toNumber() });
+      }
+    }
+  }
+  async submitBet(event) {
+    var sessionId = event.currentTarget.sessionId;
+    var bet = event.currentTarget.bet;
+    var value = event.currentTarget.sessionValue;
+    console.log(`Bet: ${bet}, SessionId: ${sessionId}, Value in ether: ${value}`);
+    try {
+      await this.bettingInstance.placeBet(sessionId, bet, { value: value });
+    }
+    catch (error) {
+      console.log("Somthing went wrong: " + error);
+    }
+    this.$.sendBet.value="";
+  }
+  async closeSession(event) {
+    var sessionId = event.currentTarget.sessionId;
+    console.log(`Closing session with id '${sessionId}'.`);
+    try {
+      await this.bettingInstance.closeSession(sessionId);
+    }
+    catch (error) {
+      console.log("Somthing went wrong: " + error);
+    }
+  }
+  async withdraw(event) {
+    var sessionId = event.currentTarget.sessionId;
+    console.log(`Withdrawing winnings form session with id '${sessionId}'.`);
+    try {
+      await this.bettingInstance.withdraw(sessionId);
+    }
+    catch (error) {
+      console.log("Somthing went wrong: " + error);
+    }
   }
 }
 
